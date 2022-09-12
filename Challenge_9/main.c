d16612fa176049ce86d536281a27380687c4c199
saad moumou <saad.moumou.coder@gmail.com>
2022-09-12 11:07:55 +0100

    I Finish Challenge 9

---
+++ Challenge_9/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int N;
+bool isPremier(N){
+    int i;
+    int estPremier = 1;
+    for(i=2; i<=N/2; i++){
+        if(N%i == 0){
+            //Faux
+            estPremier = 0;
+        }
+    }
+    //Return Value 1 or 0
+    return estPremier;
+
+}
+int main()
+{
+    printf("Entre Le Numbre: ");
+    scanf("%d",&N);
+
+    //Check Func Premier or Not
+    if(isPremier(N) == 0){
+        printf("Non Premier");
+    }else{
+        printf("Premier");
+    }
+    return 0;
+}
