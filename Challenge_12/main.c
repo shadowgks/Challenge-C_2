4e78dd1fb1005e144253a0d72178e8e10820bd24
saad moumou <saad.moumou.coder@gmail.com>
2022-09-14 17:02:18 +0100

    Fixed This Challenge

--- Challenge_12/main.c
+++ Challenge_12/main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-void strcpy(char *T[3],char *TC[3]){
-    *TC = *T;
+void strcpy(char T[],char TC[]){
+
+    //Use new var pointeur because table like constant
+    char *a = T;
+    char *b = TC;
+
+
+    //Check content T if you receipt 'end of caractere \0'
+    while(*a != '\0'){
+        //'Table TC' take info from 'Table T'
+        *b = *a;
+        //Increment
+        a++;
+        b++;
+    printf("%p\n",b);
+    }
+    *b="\0"; //important end of string case
     printf("Les Donnes Copy Dans La Table: %s",TC);
 
 }
 int main()
 {
     char T[30],TC[30];
-    printf("Entre Some Text: ");
+    printf("Entrez les Text: ");
     scanf("%s",T);
     strcpy(&T,&TC);
 
